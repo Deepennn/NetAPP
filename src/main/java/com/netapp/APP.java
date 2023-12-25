1c6edd16c6dd5d6644d2656364881c85a7b64788
Deepen <109563770+TensorTorcher@users.noreply.github.com>
2023-12-25 21:04:06 +0800

    代码优化

--- src/main/java/com/netapp/APP.java
+++ src/main/java/com/netapp/APP.java
@@ -8,72 +8,78 @@ import java.util.Scanner;
 import static com.netapp.config.DeviceConfig.*;
 
 public class APP {
-    public static void main(String[] args) {
-        /**----------------------------------SETUP-------------------------------------*/
+    public static Net net;
+    public static void setup(){
         // 创建网络
-        Net net = NetFactory.provide();
+        net = NetFactory.provide();
         // 启动网络
         new Thread(net).start();
-        /**----------------------------------------------------------------------------*/
-        /**----------------------------------SENDING-----------------------------------*/
-        // 创建Scanner对象
+    }
+    public static void service(){
+        // 创建Scanner
         Scanner scanner = new Scanner(System.in);
         System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
         System.out.println(
                 "    WELCOME TO NET LAYER!\n" +
-                "    YOU ARE NOW `h1` IN THE NET TOPO AS BELOW:\n"
-                );
+                        "    YOU ARE NOW `h1` IN THE NET TOPO AS BELOW:\n"
+        );
         System.out.println(
                 "    ```````````````````NET TOPO`````````````````\n" +
-                "    h1   <===>         r1        <===> h2\n" +
-                "    h1_i <---> [ r1_i1 , r1_i2 ] <---> h2_i\n" +
-                "    ^                                      \n" +
-                "   ```````````````````````````````````````````````````"
-                );
+                        "    h1   <===>         r1        <===> h2\n" +
+                        "    h1_i <---> [ r1_i1 , r1_i2 ] <---> h2_i\n" +
+                        "    ^                                      \n" +
+                        "   ```````````````````````````````````````````````````"
+        );
         System.out.print(
                 "    TRY SEND A MESSAGE TO `h2`!\n" +
-                "    INPUT YOUR(`h1`'s) MESSAGE HERE: "
+                        "    INPUT YOUR(`h1`'s) MESSAGE HERE: "
         );
         // 读取用户输入的 MESSAGE
         String message = scanner.nextLine();
         System.out.println("/**```````````````````YOUR MESSAGE IS SENDING!````````````````*/");
 
-
-
-
-        // h1 -> h2               @TEST: h1 -> r1 ->h2
+        /**------------------TEST ONLY ONE `@TEST` EACH TIME------------------------------*/
+        /**
+         * h1 -> h2               @TEST: h1 -> r1 ->h2
+         * */
         net.service.sendIPPacket(H1_HOSTNAME, H2_I_IP, message, 64);
 
-
-
-        // h2 -> h1               @TEST: h2 -> r1 ->h1
+        /**
+         * h2 -> h1               @TEST: h2 -> r1 ->h1
+         * */
 //        net.service.sendIPPacket(H2_HOSTNAME, H1_I_IP, message, 64);
 
-
-
-        // h1 -> r1               @TEST: h1 -> r1
+        /**
+         * h1 -> r1               @TEST: h1 -> r1
+         * */
 //        net.service.sendIPPacket(H1_HOSTNAME, R1_I1_IP, message, 64);
 
-
-
-        // h1 -> ?                @TEST: ICMP_DESTINATION_NETWORK_UNREACHABLE
+        /**
+         *  h1 -> ?                @TEST: ICMP_DESTINATION_NETWORK_UNREACHABLE
+         * */
 //        net.service.sendIPPacket(H1_HOSTNAME,"1.1.1.1", message, 64);
 
-
-
-        // h1 -> h2               @TEST: ICMP_DESTINATION_TIME_EXCEEDED
+        /**
+         *  h1 -> h2               @TEST: ICMP_DESTINATION_TIME_EXCEEDED
+         *  */
 //        net.service.sendIPPacket(H1_HOSTNAME, H2_I_IP, message, 1);
 
-
-
-         // h1 ?-> h2              @TEST: ARP_REQ
-//        BY DELETING THE ENTRY `1.0.0.1 00:11:22:33:44:55`
-//                 IN THE FILE `src/main/resources/config/arp_cache/h1.ac`
-
+        /**
+         *  h1 ?-> h2              @TEST: ARP_REQ
+         *  NOTIFICATION:
+         *  DELETE THE ENTRY `1.0.0.1 00:11:22:33:44:55`
+         *  IN THE FILE `src/main/resources/config/arp_cache/h1.ac`
+         *  MANUALLY,
+         *  MAKE SURE THERE IS <=1 BLANK LINES AT THE BOTTOM
+         *  AND TRY THIS AGAIN:
+         *  */
+//        net.service.sendIPPacket(H1_HOSTNAME, H2_I_IP, message, 64);
 
         // 关闭Scanner
         scanner.close();
-        /**----------------------------------------------------------------------------*/
-
+    }
+    public static void main(String[] args) {
+        setup();
+        service();
     }
 }
